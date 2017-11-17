936042bd927a945bda78fcf053e5689efeb07fc7
cjsv <carlojay.sv@gmail.com>
2017-11-17 18:10:14 +0800

    Initialized project files.

---
+++ app/src/main/java/com/github/cj_sv/coffeeshop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.cj_sv.coffeeshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
